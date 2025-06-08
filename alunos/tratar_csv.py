97efece42c886e288742d60f3490ba9f127f5724
Matheus51bento <matheus51bento@gmail.com>
2025-06-08 08:55:58 -0300

    gerando simulações

--- alunos/tratar_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("aluno2.csv")
-
-df["peso_por_questao"] = df["peso_por_questao"].str.replace(",", ".").astype(float)
-df["peso_subclasse"] = df["peso_subclasse"].str.replace(",", ".").astype(float)
-
-df.to_csv("aluno2.csv", index=False)
